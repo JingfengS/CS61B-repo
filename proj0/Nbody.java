cd43c11863cd2797d5cd105737ab91a9314d27b1
JingfengS <jingfeng050106@gmail.com>
2023-07-11 16:47:43 +0800

    Modify the file to make it get used to autograder.

--- proj0/Nbody.java
+++ proj0/Nbody.java
@@ -1,31 +1,32 @@
 public class NBody{
 
+	/** the main working loop */
 	public static void main(String[] args){
 		double T = Double.parseDouble(args[0]);
 		double dt = Double.parseDouble(args[1]);
 		String fileName = args[2];
-		Planet[] Planets = readPlanets(fileName);
-		double radiusUniverse = readRadius(fileName);
+		Planet[] planets = readplanets(fileName);
+		double radius = readRadius(fileName);
 
-		StdDraw.setScale(-1 * radiusUniverse, radiusUniverse);
+		StdDraw.setScale(-1 * radius, radius);
 		StdDraw.enableDoubleBuffering();
 
 		for (int time = 0; time < T; time += dt){
-			double[] xForces = new double[Planets.length];
-			double[] yForces = new double[Planets.length];
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
 
-			for (int i = 0; i < Planets.length; i += 1){
-				xForces[i] = Planets[i].calcNetForceExertedByX(Planets);
-				yForces[i] = Planets[i].calcNetForceExertedByY(Planets);
+			for (int i = 0; i < planets.length; i += 1){
+				xForces[i] = planets[i].calcNetForceExertedByX(planets);
+				yForces[i] = planets[i].calcNetForceExertedByY(planets);
 			}
 
-			for (int i = 0; i < Planets.length; i += 1){
-				Planets[i].update(dt, xForces[i], yForces[i]);
+			for (int i = 0; i < planets.length; i += 1){
+				planets[i].update(dt, xForces[i], yForces[i]);
 			}
 
 			StdDraw.clear();
 			StdDraw.picture(0, 0, "images/starfield.jpg"); 
-			for (Planet p: Planets){
+			for (Planet p: planets){
 				p.draw();
 			}
 
@@ -55,24 +56,24 @@ public class NBody{
 	/**
 	 * Return an array of all the planets in the universe
 	 */
-	public static Planet[] readPlanets(String fileName){
+	public static Planet[] readplanets(String fileName){
 		In in = new In(fileName);
 
-		int totalPlanets = in.readInt();
+		int totalplanets = in.readInt();
 		in.readDouble();
 
 
-		Planet[] Planets = new Planet[totalPlanets];
-		for (int i = 0; i < totalPlanets; i += 1){
+		Planet[] planets = new Planet[totalplanets];
+		for (int i = 0; i < totalplanets; i += 1){
 			double xxPos = in.readDouble();
 			double yyPos = in.readDouble();
 			double xxVel = in.readDouble();
 			double yyVel = in.readDouble();
 			double mass = in.readDouble();
 			String imgFileName = in.readString();
-			Planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);		       }
+			planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);		       }
 		
-		return Planets;
+		return planets;
 	}
 
 }
