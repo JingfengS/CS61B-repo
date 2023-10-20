8765581fc66fd18a2deda0db5d183469928c82c0
JingfengS <jingfeng050106@gmail.com>
2023-10-20 16:48:13 +0800

    Initialize Percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,53 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    /**
+     * create N-by=N grid, with all sites initially blocked
+     */
+    public Percolation(int N) {
+
+    }
+
+    /**
+     * open the site (row, col) if it is not open already
+     * @param row row number of the sites to be opened
+     * @param col column number of the sites to be opened
+     */
+    public void open(int row, int col) {
+
+    }
+
+    /**
+     * is the site (row, col) open?
+     * @param row
+     * @param col
+     */
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    /**
+     * is the site (row, col) full?
+     * @param row
+     * @param col
+     */
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    /**
+     * @return number of the open sites
+     */
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    /**
+     * dose the system percolate?
+     */
+    public boolean percolates() {
+        return false;
+    }
+
+
 }
