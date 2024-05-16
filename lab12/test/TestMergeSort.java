ea798b4b6f63505d9e8d05ffe4cd99523b331200
JingfengS <jingfeng050106@gmail.com>
2024-05-16 20:51:25 +0800

    Start working on lab12

---
+++ lab12/test/TestMergeSort.java
@@ -0,0 +1,10 @@
+package test;
+import static com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class TestMergeSort {
+    @Test
+    public void test() {
+    }
+}
+
---
+++ lab12/test/TestQuickSort.java
@@ -0,0 +1,5 @@
+package test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class TestQuickSort {
+}
